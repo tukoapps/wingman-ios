4c0f3162f165955d1d2e6f9eec339b53af1f99ea
Stephen Chan <stephenchan2015@u.northwestern.edu>
2014-08-08 17:40:21 -0500

    add rating assets and custom view xib for bar view

---
+++ WingMan/WingMan/WMBarCellView.h
@@ -0,0 +1,13 @@
+//
+//  WMBarCellView.h
+//  WingMan
+//
+//  Created by Stephen Chan on 8/8/14.
+//  Copyright (c) 2014 TukoApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMBarCellView : UIView
+
+@end
---
+++ WingMan/WingMan/WMBarCellView.m
@@ -0,0 +1,31 @@
+//
+//  WMBarCellView.m
+//  WingMan
+//
+//  Created by Stephen Chan on 8/8/14.
+//  Copyright (c) 2014 TukoApps. All rights reserved.
+//
+
+#import "WMBarCellView.h"
+
+@implementation WMBarCellView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
